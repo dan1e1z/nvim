6f3271a8e56be6be12b8dd239e32bad66c093250
Daniel Lindsay-Shad <dlin8156@uni.sydney.edu.au>
2024-06-20 05:37:25 +1000

    move to last cursor position autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,17 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 		vim.lsp.buf.format({ name = "null-ls", async = true })
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function(event)
+		local buf = event.buf
+		local mark = vim.api.nvim_buf_get_mark(buf, '"')
+		local lcount = vim.api.nvim_buf_line_count(buf)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+			vim.schedule(function()
+				vim.cmd("normal! zz")
+			end)
+		end
+	end,
+})
