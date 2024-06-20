0ef5ee3bd51fc43a09697384eccbdb87eb8ab00e
Daniel Lindsay-Shad <dlin8156@uni.sydney.edu.au>
2024-06-20 12:16:51 +1000

    changed harpoon keybinding

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,10 +2,22 @@ return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
-    vim.keymap.set('n', '<leader>h', function()require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())end),
-    vim.keymap.set('n', '<leader>m', function()require('harpoon'):list():add()end),
-    vim.keymap.set('n', '<S-h>', function()require('harpoon'):list():select(1)end),
-    vim.keymap.set('n', '<S-j>', function()require('harpoon'):list():select(2)end),
-    vim.keymap.set('n', '<S-k>', function()require('harpoon'):list():select(3)end),
-    vim.keymap.set('n', '<S-l>', function()require('harpoon'):list():select(4)end),
+	vim.keymap.set("n", "<leader>h", function()
+		require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+	end),
+	vim.keymap.set("n", "<leader>a", function()
+		require("harpoon"):list():add()
+	end),
+	vim.keymap.set("n", "<S-h>", function()
+		require("harpoon"):list():select(1)
+	end),
+	vim.keymap.set("n", "<S-j>", function()
+		require("harpoon"):list():select(2)
+	end),
+	vim.keymap.set("n", "<S-k>", function()
+		require("harpoon"):list():select(3)
+	end),
+	vim.keymap.set("n", "<S-l>", function()
+		require("harpoon"):list():select(4)
+	end),
 }
