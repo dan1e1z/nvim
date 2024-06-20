9ebb56d0c8d268267d364e2cce6d46c6fafd6c98
Daniel Lindsay-Shad <dlin8156@uni.sydney.edu.au>
2024-06-20 11:54:54 +1000

    edited noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,7 +5,7 @@ return {
 		routes = {
 			{
 				view = "notify",
-				filter = { event = "msg_showmode" },
+				filter = { event = "msg_show" },
 			},
 		},
 		lsp = {
@@ -17,13 +17,13 @@ return {
 			},
 		},
 		-- you can enable a preset for easier configuration
-		presets = {
-			bottom_search = true, -- use a classic bottom cmdline for search
-			command_palette = true, -- position the cmdline and popupmenu together
-			long_message_to_split = true, -- long messages will be sent to a split
-			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false, -- add a border to hover docs and signature help
-		},
+		-- presets = {
+		-- 	bottom_search = true, -- use a classic bottom cmdline for search
+		-- 	command_palette = true, -- position the cmdline and popupmenu together
+		-- 	long_message_to_split = true, -- long messages will be sent to a split
+		-- 	inc_rename = false, -- enables an input dialog for inc-rename.nvim
+		-- 	lsp_doc_border = false, -- add a border to hover docs and signature help
+		-- },
 	},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
