3e634e3f5a7d1b8e1d146d347d2fe1cc1673f68b
Daniel Lindsay-Shad <dlin8156@uni.sydney.edu.au>
2024-06-20 12:16:19 +1000

    removed neotree keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,5 @@
 local opts = { noremap = true, silent = true }
 
--- NeoTree
-vim.keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", opts) -- Focus on NvimTree
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts) -- Toggle NvimTree visibility
-
 -- Window Management
 vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
 vim.keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally
