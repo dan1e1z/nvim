3f3431f5cb1e9ac4bfbb64bf6061c0b9d30af3b3
Daniel Lindsay-Shad <dlin8156@uni.sydney.edu.au>
2024-06-20 11:56:23 +1000

    edited util icons

--- lua/util/icons.lua
+++ lua/util/icons.lua
@@ -1,11 +1,17 @@
 local M = {}
+-- Set highlight groups for DAP signs
+vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "NONE" })
+vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "NONE" })
+vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "NONE" })
+vim.api.nvim_set_hl(0, "DapBreakpointCondition", { ctermbg = 0, fg = "#E5C07B", bg = "NONE" })
+vim.api.nvim_set_hl(0, "DapBreakpointRejected", { ctermbg = 0, fg = "#E06C75", bg = "NONE" })
 
 M.debugging_signs = {
-	Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
-	Breakpoint = " ",
-	BreakpointCondition = " ",
-	BreakpointRejected = { " ", "DiagnosticError" },
-	LogPoint = ".>",
+	Stopped = { text = "󰁕", texthl = "DapStopped", linehl = "", numhl = "" },
+	Breakpoint = { text = "", texthl = "DapBreakpoint", linehl = "", numhl = "" },
+	BreakpointCondition = { text = "", texthl = "DapBreakpointCondition", linehl = "", numhl = "" },
+	BreakpointRejected = { text = " ", texthl = "DapBreakpointRejected", linehl = "", numhl = "" },
+	LogPoint = { test = ".>", texthl = "DapLogPoint", linehl = "", numhl = "" },
 }
 
 M.diagnostic_signs = {
