6835ee6945d32ea7c4cce5e805deae68351a979e
Daniel Lindsay-Shad <dlin8156@uni.sydney.edu.au>
2024-06-20 11:56:00 +1000

    edited trouvle config

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,5 +1,16 @@
+local diagnostic_signs = require("util.icons").diagnostic_signs
+
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
+	opts = {
+		signs = {
+			-- icons / text used for a diagnostic
+			error = diagnostic_signs.Error,
+			warning = diagnostic_signs.Warn,
+			hint = diagnostic_signs.Hint,
+			information = diagnostic_signs.Info,
+			other = diagnostic_signs.Info,
+		},
+	},
 }
