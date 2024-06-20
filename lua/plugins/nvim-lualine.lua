587985b693d485eeea0c6b543382132fd970660f
Daniel Lindsay-Shad <dlin8156@uni.sydney.edu.au>
2024-06-20 12:17:21 +1000

    fixed lualine

--- lua/plugins/nvim-lualine.lua
+++ lua/plugins/nvim-lualine.lua
@@ -5,16 +5,16 @@ return {
 		require("lualine").setup({
 			sections = {
 				lualine_x = {
-				  {
-				    require("noice").api.status.command.get,
-				    cond = require("noice").api.status.command.has,
-				    color = { fg = "#ff9e64" },
-				  },
-				  {
-				    require("noice").api.status.mode.get,
-				    cond = require("noice").api.status.mode.has,
-				    color = { fg = "#ff9e64" },
-				  },
+					{
+						require("noice").api.status.command.get,
+						cond = require("noice").api.status.command.has,
+						color = { fg = "#ff9e64" },
+					},
+					{
+						require("noice").api.status.mode.get,
+						cond = require("noice").api.status.mode.has,
+						color = { fg = "#ff9e64" },
+					},
 				},
 			},
 		})
