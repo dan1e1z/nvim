251da5a4f6bba39398e614880e3e9277a45ab396
Daniel Lindsay-Shad <dlin8156@uni.sydney.edu.au>
2024-06-20 12:17:42 +1000

    moved neotree keybindings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -9,5 +9,11 @@ return {
 				dotfiles = false,
 			},
 		})
+
+		local opts = { noremap = true, silent = true }
+
+		-- NeoTree
+		vim.keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", opts) -- Focus on NvimTree
+		vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts) -- Toggle NvimTree visibility
 	end,
 }
